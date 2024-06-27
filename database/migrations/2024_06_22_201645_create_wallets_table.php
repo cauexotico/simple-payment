afafb1f83d9a601b6c5f73302b4f154dcb644a04
Khaue Poerner <khaue.p@grupovvale.local>
2024-06-27 11:49:24 -0300

    feature: migration for wallets

---
+++ database/migrations/2024_06_22_201645_create_wallets_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use SimplePayment\Enums\Currency;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('wallets', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuidMorphs('holder');
+            $table->unsignedBigInteger('balance');
+            $table->unsignedBigInteger('blocked_balance');
+            $table->timestamps();
+
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('wallet');
+    }
+};
